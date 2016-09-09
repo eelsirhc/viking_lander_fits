922234acbd7af5928350f021bb32d8910e61837f
Mark I. Richardson <markianrichardson@gmail.com>
2016-09-09 04:04:36 -0700

    Namelists missing custom_ice flag.
    	new file:   simulations/do_lander.sh
    	modified:   simulations/template/namelist.input
    	modified:   simulations/template/namelist.input.mir_marsgcm_fixed_dust_no_water_new_surface_for_p_fitting_2016_09
    	modified:   simulations/template/namelist.input.mir_marsgcm_fixed_dust_no_water_old_surface_for_p_fitting_2016_09

---
+++ simulations/do_lander.sh
@@ -0,0 +1,12 @@
+./viking_lander.py base/wrfout_d01_000* --output v1.base.data
+./viking_lander.py co2_high/wrfout_d01_000* --output v1.co2hi.data
+./viking_lander.py co2_low/wrfout_d01_000* --output v1.co2low.data
+./viking_lander.py nh_albedo_high/wrfout* --output v1.nhalbhi.data
+./viking_lander.py nh_albedo_low/wrfout* --output v1.nhalblo.data
+./viking_lander.py nh_emiss_high/wrfout* --output v1.nhemihi.data
+./viking_lander.py nh_emiss_low/wrfout* --output v1.nhemilo.data
+./viking_lander.py sh_albedo_high/wrfout* --output v1.shalbhi.data
+./viking_lander.py sh_albedo_low/wrfout* --output v1.shalblo.data
+./viking_lander.py sh_emiss_high/wrfout* --output v1.shemihi.data
+./viking_lander.py sh_emiss_low/wrfout* --output v1.shemilo.data
+
